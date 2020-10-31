a937382bad24f14bf257f2cc3c30c7eeabcf94e3
Michael Kline <klinem@gmail.com>
2020-10-31 10:55:22 -0600

    photoshop gallery and header complete

---
+++ photoshop/js/javascript.js
@@ -0,0 +1,25 @@
+/*
+	program heading: Photoshop gallery
+	author name: Mike Kline
+	version: 0.5
+	description: a gallery to display my photoshop images
+	*/
+
+var imageSlide = ["images/My890Jframed.jpg", "images/MadnessInTheRockiesframed.jpg", "images/InTimeAndSpaceframed.jpg"]
+var index = 0;
+
+function next() {
+  if (index < 2) { //stops next function at the end
+    index++;
+    let defaultImg = document.getElementById("slide")
+    defaultImg.src = imageSlide[index];
+  }
+}
+
+function Previous() {
+  if (index > 0) { //stops previous function at the beginning
+    index--;
+    let defaultImg = document.getElementById("slide")
+    defaultImg.src = imageSlide[index];
+  }
+}
