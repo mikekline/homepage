c7ef8b2a217b797e12c6027da383a72465be5784
Michael Kline <klinem@gmail.com>
2020-12-08 03:08:00 -0700

    small change

--- js/javascript.js
+++ js/javascript.js
@@ -19,11 +19,11 @@ function Previous() {
 
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function menu() {
   var x = document.getElementById("menu");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
-} 
+}  
